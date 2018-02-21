4b8c27214ecd9b1e2b3568d34578ec4ba493c315
leahewers <leahewers@172.16.21.57>
2018-02-21 10:34:04 -0500

    Leah's lights out game outline so far

---
+++ src/LightButton.java
@@ -0,0 +1,15 @@
+
+public class LightButton {
+	private static int SIZE = 100; 
+	boolean isOn= false; 
+	
+	public LightButton() {
+		isOn = false; 	
+	}
+	
+	public boolen get isOn; 
+	
+	public void foggle () 
+	
+
+}
---
+++ src/LightsOut.java
@@ -0,0 +1,29 @@
+
+public class LightsOut {
+	private static final int GRIDSIZE = 10; 
+	
+	LightButton [] []
+			
+	lightBoard= new; 
+	
+	Public lightsOut () {
+		
+	}
+	
+	Private void 
+	
+	Private void promptForNewGame (message); 
+	String message= "new game"; 
+
+	private void initGUI() {
+		
+	}
+	
+	public void boardSetup()
+	
+	public void button(int r, int c) {
+		row = r;
+		col = c; 
+	} 
+		
+}
